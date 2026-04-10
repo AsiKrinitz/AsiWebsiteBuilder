0157b1783aada5aab62b43cda7bc0bcfde62aa22
AsiKrinitz <AsiKrinitz@gmail.com>
2026-04-10 16:57:23 +0300

    adding the footer with the links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,44 @@
+import "./Footer.css";
+
 export function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="footer-content">
-          <div className="logo">
-            <span className="logo-accent">ASI</span> DESIGN
-          </div>
-          <p>בניית אתרים שבאמת עובדים בשבילך.</p>
-        </div>
-        <div className="footer-bottom">
-          <p>© {new Date().getFullYear()} ASI DESIGN | אסי קריניץ. כל הזכויות שמורות.</p>
-        </div>
-      </div>
+      <h2>כל הזכויות שמורות לאסי קריניץ</h2>
+
+      <ul className="social-links">
+        <li>
+          <a
+            href="https://www.facebook.com/ShahafKrinitz/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            פייסבוק&nbsp;
+            <img alt="Facebook icon" src="Facebook_logo.png" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://www.instagram.com/asikrinitz/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            אינסטגרם&nbsp;
+            <img alt="Instagram icon" src="Instagram_logo.png" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://www.linkedin.com/in/asi-krinitz-25351223b/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            לינקדאין&nbsp;
+            <img alt="LinkedIn icon" src="Linkedin_logo.png" />
+          </a>
+        </li>
+      </ul>
     </footer>
-  )
+  );
 }
